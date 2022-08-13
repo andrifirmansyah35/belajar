1c00e87c6378d0fad6a519ea9754acee9f333476
andrifirmansyah35 <ahsyandri@gmail.com>
2022-08-13 18:41:41 +0700

    menambah file belajar ui/ux

---
+++ Dikoding/Belajar FrontEnd Web Untuk Pemula/Dasar Javascript/TipeData.js
@@ -0,0 +1,15 @@
+// // 1M.Menggunakna Tipre data
+// let x
+// console.log(typeof(x))
+// // typeoff mengembalikan tipe dari x
+
+
+// // 2. Number
+// let x = 10
+// console.log(typeof(x))
+
+// console.log(5n + 2n);
+// console.log(5n - 2n);
+// console.log(5n * 2n);
+// console.log(5n / 2n);
+// console.log(5n % 2n);
